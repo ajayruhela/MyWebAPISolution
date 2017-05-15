c72c4b877e1146638181e3edb82b746b9ff2f7f0
ajayruhela <jrhl0013@gmail.com>
2017-05-15 15:55:25 +0200

    initial changes

---
+++ HelloWebAPI/Controller/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+
+namespace HelloWebAPI.Controller
+{
+
+        public class HelloController : ApiController
+        {
+            public string Get()
+            {
+                return "Hello World";
+            }
+        }
+    
+}
